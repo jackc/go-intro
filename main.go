bfa56453c0a64db476329dc0fe1234f5131ee960
Jack Christensen <jack@jackchristensen.com>
2013-11-29 15:20:08 -0600

    Simple curl clone

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Args)
-	fmt.Println("Hello, world!")
+	response, err := http.Get(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+
+	_, err = io.Copy(os.Stdout, response.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
